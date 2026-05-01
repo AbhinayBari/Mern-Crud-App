fa3465bcfbeba6de10d97bb171362cf2694bebab
Abhinay Bari <abhinaybari794@gmail.com>
2026-05-01 19:09:17 +0530

    Fix deployment line error

--- frontend/src/UpdateUser.jsx
+++ frontend/src/UpdateUser.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from "react";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
